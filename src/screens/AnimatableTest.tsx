162b0012d36317a96ca7339f2c296cb345108dfc
parksangryong <parksangryong@Mac-mini-2.local>
2024-10-16 12:00:54 +0900

    예시추가

--- src/screens/AnimatableTest.tsx
+++ src/screens/AnimatableTest.tsx
@@ -10,41 +10,101 @@ const AnimatableTest = () => {
     <ScrollView>
       <View style={styles.container}>
         <Animatable.View
-          animation="slideInLeft"
+          animation="bounce"
           duration={1000}
           style={{ width: 100, height: 100, backgroundColor: "red" }}
         ></Animatable.View>
         <Animatable.View
-          animation="bounce"
-          duration={2000}
+          animation="flash"
+          duration={1000}
           delay={1000}
           style={{ width: 100, height: 100, backgroundColor: "pink" }}
         ></Animatable.View>
         <Animatable.View
           animation="jello"
           duration={3000}
-          delay={3000}
+          delay={1500}
           style={{ width: 100, height: 100, backgroundColor: "brown" }}
         ></Animatable.View>
       </View>
       <View style={styles.container}>
-        <Animatable.Text
+        <Animatable.View
+          animation="pulse"
+          duration={1000}
+          delay={2000}
+          style={{ width: 100, height: 100, backgroundColor: "yellowgreen" }}
+        ></Animatable.View>
+        <Animatable.View
+          animation="rotate"
+          duration={2000}
+          delay={2500}
+          style={{ width: 100, height: 100, backgroundColor: "green" }}
+        ></Animatable.View>
+        <Animatable.View
+          animation="rubberBand"
+          duration={3000}
+          delay={3000}
+          style={{ width: 100, height: 100, backgroundColor: "olive" }}
+        ></Animatable.View>
+      </View>
+      <View style={styles.container}>
+        <Animatable.View
+          animation="shake"
+          duration={1000}
+          delay={3500}
+          style={{ width: 100, height: 100, backgroundColor: "skyblue" }}
+        ></Animatable.View>
+        <Animatable.View
+          animation="swing"
+          duration={2000}
+          delay={4000}
+          style={{ width: 100, height: 100, backgroundColor: "blue" }}
+        ></Animatable.View>
+        <Animatable.View
+          animation="wobble"
+          duration={3000}
+          delay={4500}
+          style={{ width: 100, height: 100, backgroundColor: "navy" }}
+        ></Animatable.View>
+      </View>
+      <View style={styles.container}>
+        <Animatable.View
+          animation="slideInLeft"
+          duration={1000}
+          delay={5000}
+          style={{ width: 100, height: 100, backgroundColor: "yellow" }}
+        ></Animatable.View>
+        <Animatable.View
           animation="fadeIn"
+          duration={2000}
+          delay={6000}
+          style={{ width: 100, height: 100, backgroundColor: "gold" }}
+        ></Animatable.View>
+        <Animatable.View
+          animation="zoomIn"
+          duration={3000}
+          delay={7000}
+          style={{ width: 100, height: 100, backgroundColor: "orange" }}
+        ></Animatable.View>
+      </View>
+      <View style={styles.container}>
+        <Animatable.Text
+          animation="zoomInRight"
           duration={1000}
           delay={1000}
           style={{ fontSize: 20, color: "green" }}
         >
-          Hello
+          zoomInRight, Hello
         </Animatable.Text>
       </View>
       <View style={styles.container}>
         <Animatable.Text
-          animation="zoomIn"
+          animation="flipInX"
           duration={1000}
           delay={1000}
           style={{ fontSize: 20, color: "green" }}
         >
-          Hello
+          flipInX, Hello
         </Animatable.Text>
       </View>
     </ScrollView>
--- src/screens/ReanimatedTest.tsx
+++ src/screens/ReanimatedTest.tsx
@@ -1,8 +1,10 @@
-import { View, ScrollView, Button, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, ScrollView, Button, StyleSheet, Text } from "react-native";
 import {
   Gesture,
   GestureDetector,
   GestureHandlerRootView,
+  PanGestureHandler,
 } from "react-native-gesture-handler";
 
 import Animated, {
@@ -28,6 +30,9 @@ const ReanimatedTest = () => {
   const translateX3 = useSharedValue(0);
 
   const pressed = useSharedValue<boolean>(false);
+
+  const isVisible = useSharedValue(true);
+  const opacity = useSharedValue(0);
 
   //애니메이션1
   const pregressPress = (type: "plus" | "minus") => {
@@ -102,12 +107,48 @@ const ReanimatedTest = () => {
   const animatedStyles = useAnimatedStyle(() => ({
     backgroundColor: pressed.value ? "#FFE04B" : "#B58DF1",
     transform: [{ scale: withTiming(pressed.value ? 1.2 : 1) }],
+    reduceMotion: ReduceMotion.Never,
   }));
+
+  //애니메이션5
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: opacity.value,
+    };
+  });
+
+  const toggleVisibility = () => {
+    opacity.value = withTiming(isVisible.value ? 0 : 1, {
+      duration: 1000,
+      reduceMotion: ReduceMotion.Never,
+    });
+    isVisible.value = !isVisible.value; // 상태 반전
+  };
+
+  //애니메이션6 : 드래그 앤 드롭
+  const translateX4 = useSharedValue(0);
+  const translateY4 = useSharedValue(0);
+
+  const animatedStyle6 = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX4.value },
+        { translateY: translateY4.value },
+      ],
+    };
+  });
+
+  const onDrag = (event: any) => {
+    const { translationX, translationY } = event.nativeEvent;
+    translateX4.value = translationX;
+    translateY4.value = translationY;
+  };
 
   return (
     <ScrollView style={styles.container}>
       {/* 크기 변화 */}
       <View style={styles.firstView}>
+        <Text style={styles.text}> withSpring</Text>
         <Animated.View
           style={{
             width,
@@ -123,6 +164,7 @@ const ReanimatedTest = () => {
       </View>
       {/* 옆으로 밀기 */}
       <View style={styles.secondView}>
+        <Text style={styles.text}> withSpring2</Text>
         <Animated.View
           style={{
             width: 100,
@@ -139,6 +181,7 @@ const ReanimatedTest = () => {
       </View>
       {/* 옆으로 굴러가기 */}
       <View style={styles.thirdView}>
+        <Text style={styles.text}> withTiming & withSpring</Text>
         <Animated.View
           style={{
             width: 40,
@@ -163,10 +206,28 @@ const ReanimatedTest = () => {
       </View>
       {/* 애니메이션4 */}
       <View style={styles.fourthView}>
+        <Text style={styles.text}>gestureTap</Text>
         <GestureHandlerRootView style={{ flex: 1 }}>
           <GestureDetector gesture={tap}>
             <Animated.View style={[styles.circle, animatedStyles]} />
           </GestureDetector>
+        </GestureHandlerRootView>
+      </View>
+      {/* 애니메이션5 */}
+      <View style={styles.fifthView}>
+        <Text style={styles.text}>Toggle Box</Text>
+        <Button title="Toggle Box" onPress={toggleVisibility} />
+        {isVisible.value && (
+          <Animated.View style={[styles.box, animatedStyle]} />
+        )}
+      </View>
+      {/* 애니메이션6 */}
+      <View style={styles.sixthView}>
+        <Text style={styles.text}>gestureDrag</Text>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+          <PanGestureHandler onGestureEvent={onDrag}>
+            <Animated.View style={[styles.box2, animatedStyle6]} />
+          </PanGestureHandler>
         </GestureHandlerRootView>
       </View>
       <View style={{ height: 50 }} />
@@ -220,6 +281,39 @@ const styles = StyleSheet.create({
     width: 40,
     height: 40,
     borderRadius: 20,
+  },
+  fifthView: {
+    alignItems: "center",
+    marginBottom: 20,
+    backgroundColor: "white",
+    overflow: "hidden",
+    paddingVertical: 20,
+    gap: 10,
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+    borderRadius: 10,
+  },
+  sixthView: {
+    alignItems: "center",
+    marginBottom: 20,
+    backgroundColor: "white",
+    overflow: "hidden",
+    paddingVertical: 20,
+  },
+  box2: {
+    width: 50,
+    height: 50,
+    backgroundColor: "beige",
+    borderRadius: 25,
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+    alignSelf: "center",
   },
 });
 
